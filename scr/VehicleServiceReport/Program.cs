6f21b6906463cadd37ef89dbfdb7ce007dffbdff
Victor Henao <victorhenao@gmail.com>
2016-11-02 22:46:15 -0500

    Changes on the code to prevent dirty results from one execution in the following one. Make results from view excution independent from result of stored procedure execution.

--- scr/VehicleServiceReport/Program.cs
+++ scr/VehicleServiceReport/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using VehicleServiceReport.DataSource;
 
 namespace VehicleServiceReport
@@ -9,27 +10,55 @@ namespace VehicleServiceReport
     {
         static void Main(string[] args)
         {
-            VehicleServiceDataAccess vehicleData = new VehicleServiceDataAccess();
+            int iterations = 100;
+            List<TimeSpan> viewTimes = new List<TimeSpan>();
+            List<TimeSpan> storedProcedureTimes = new List<TimeSpan>();
+            double averageTimeForViews;
+            double averageTimeForStoredProcedures;
+            for (int currentIteration = 0; currentIteration < iterations; currentIteration++)
+            {
+                Console.WriteLine("Current Iteration: {0} of {1} Iterations.", currentIteration, iterations);                
+                storedProcedureTimes.Add(InvokeStoredProcedure());
+                viewTimes.Add(InvokeView());
+                Console.Clear();
+            }
+            averageTimeForViews = viewTimes.Average(time => time.TotalMilliseconds);
+            averageTimeForStoredProcedures = storedProcedureTimes.Average(time => time.TotalMilliseconds);
+            Console.WriteLine("Time elapsed with the view: {0} Milliseconds.", averageTimeForViews);
+            Console.WriteLine("Time elapsed with the stored procedure: {0} Milliseconds.", averageTimeForStoredProcedures);
+            Console.ReadKey();
+        }
+
+        static TimeSpan InvokeView()
+        {
             Stopwatch stopwatch = new Stopwatch();
             TimeSpan timeUsedToGetFromView;
+            VehicleServiceDataAccess vehicleData = new VehicleServiceDataAccess();
             List<VehicleServiceComplete> resultFromView;
-            TimeSpan timeUsedToGetFromStoredProcedure;
-            List<uspVehicleServiceComplete> resultFromStoredProcedure;
+
             ///Measure time used to get data from the View
             stopwatch.Start();
             resultFromView = vehicleData.GetDataFromView();
             stopwatch.Stop();
             timeUsedToGetFromView = stopwatch.Elapsed;
             stopwatch.Reset();
-            ///Measure time used to get data from the Stored Procedure
+            return timeUsedToGetFromView;
+        }
+
+        static TimeSpan InvokeStoredProcedure()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            TimeSpan timeUsedToGetFromStoredProcedure;
+            VehicleServiceDataAccess vehicleData = new VehicleServiceDataAccess();
+            List<uspVehicleServiceComplete> resultFromStoredProcedure;
+
+            ///Measure time used to get data from the Stored Procedure            
             stopwatch.Start();
             resultFromStoredProcedure = vehicleData.GetDataFromStoredProcedure();
             stopwatch.Stop();
             timeUsedToGetFromStoredProcedure = stopwatch.Elapsed;
-
-            Console.WriteLine("Time elapsed with the view: {0} Milliseconds. Result: {1} records.", timeUsedToGetFromView.TotalMilliseconds, resultFromView.Count);
-            Console.WriteLine("Time elapsed with the stored procedure: {0} Milliseconds. Result: {1} records.", timeUsedToGetFromStoredProcedure.TotalMilliseconds, resultFromStoredProcedure.Count);
-            Console.ReadKey();
+            stopwatch.Reset();
+            return timeUsedToGetFromStoredProcedure;
         }
     }
 }
